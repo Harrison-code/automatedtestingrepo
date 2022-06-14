2c71527ec451169d4412c4e9c85d22dc04ce7983
Adrian <eydre@outlook.com>
2022-06-14 23:04:56 +0800

    test4

--- test1.js
+++ test1.js
@@ -1,3 +1,3 @@
-tables = document.getElementsByTagName('table');
+tables = document.getElementsByTagName('table').getElementsByTagName('tr')[0].getElementsByTagName('td')[1].getElementsByTagName('a').click();
 
 console.log(tables);
