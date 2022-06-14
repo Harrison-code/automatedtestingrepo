54d3ca8614c714a4439a027c887a7e1793837b37
Harrison-code <harrisondmix@gmail.com>
2022-06-14 23:08:16 +0800

    Hallo

--- automatedtest.js
+++ automatedtest.js
@@ -4,3 +4,4 @@ function getElementByXpath(path) {
 
 console.log( getElementByXpath("/html/body/div[1]/main/div[2]/div/div/div/div[2]/div/table/tbody/tr/td[2]/span/span[2]/a") );
 
+getElementByXpath("/html/body/div[1]/main/div[2]/div/div/div/div[2]/div/table/tbody/tr/td[2]/span/span[2]/a").click();
