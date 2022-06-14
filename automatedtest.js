6a738e2fa0f8907e64dc47f18d814548d437aa29
Harrison-code <harrisondmix@gmail.com>
2022-06-14 23:48:49 +0800

    Hello

--- automatedtest.js
+++ automatedtest.js
@@ -8,7 +8,7 @@ getElementByXpath("/html/body/div[1]/main/div[2]/div/div/div/div[2]/div/table/tb
 
 (function() {
 
-    var alvo = getElementByXpath("/html/body/div[1]/main/div[2]/div/div/div/div[2]/div/table/tbody/tr/td[2]/span/span[2]/a");
+    var alvo = getElementByXpath("/html/body/div[1]/main/div[4]/div/div/div/div[2]/section/div/div/div[2]/div/div[1]/table/tr/td[1]/strong");
     var observer = new MutationObserver(fnCheckChanges);
     observer.observe(alvo, { attributes: true, characterData: true, childList: true, subtree: true });
 
@@ -18,6 +18,6 @@ function fnCheckChanges(changes, observer) {
 
     // your code here
 
-    console.log('page changed');
+    console.log('HELLO FUCKERS');
 
 }
