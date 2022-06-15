399743a33a3dd05477e8757a262b7e9cc730cd57
Harrison-code <harrisondmix@gmail.com>
2022-06-15 18:03:40 +0800

    Hello

--- automatedtest.js
+++ automatedtest.js
@@ -6,18 +6,9 @@ console.log( getElementByXpath("/html/body/div[1]/main/div[2]/div/div/div/div[2]
 
 getElementByXpath("/html/body/div[1]/main/div[2]/div/div/div/div[2]/div/table/tbody/tr/td[2]/span/span[2]/a").click();
 
-(function() {
+const observer = new MutationObserver(entries => {
+    console.log(entries);
+});
 
-    var alvo = getElementByXpath("/html/body/div[1]/main/div[4]/div/div/div/div[2]/section/div/div/div[2]/div/div[1]/table/tr/td[1]/strong")[1];
-    var observer = new MutationObserver(fnCheckChanges);
-    observer.observe(alvo, { attributes: true, characterData: true, childList: true, subtree: true });
-
-})();
-
-function fnCheckChanges(changes, observer) {
-
-    // your code here
-
-    console.log('HELLO FUCKERS');
-
-}
+var alvo = getElementByXpath("/html/body/div[1]/main/div[4]/div/div/div/div[2]");
+observer.observe(alvo, { attributes: true, characterData: true, childList: true, subtree: true });
