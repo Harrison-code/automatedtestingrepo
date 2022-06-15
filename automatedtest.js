ff449d788237fe0bd46cb1740673f33e7a3f3c69
Harrison-code <harrisondmix@gmail.com>
2022-06-15 18:12:38 +0800

    A

--- automatedtest.js
+++ automatedtest.js
@@ -6,9 +6,11 @@ console.log( getElementByXpath("/html/body/div[1]/main/div[2]/div/div/div/div[2]
 
 getElementByXpath("/html/body/div[1]/main/div[2]/div/div/div/div[2]/div/table/tbody/tr/td[2]/span/span[2]/a").click();
 
+
 const observer = new MutationObserver(entries => {
     console.log(entries);
 });
 
-var alvo = getElementByXpath("/html/body/div[1]/main/div[4]/div/div/div/div[2]");
-observer.observe(alvo, { attributes: true, characterData: true, childList: true, subtree: true });
+const alvo = document.querySelector("#app > main > div.vue-portal-target > div > div > div > div.pt-5.p-6.bg-white.dark\\:bg-gray-700.shadow.relative");
+console.log(alvo);
+// observer.observe(alvo, { attributes: true, characterData: true, childList: true, subtree: true });
